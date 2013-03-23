3844ee1d167ca8f76ab86638678b304919debc2f
Chris Blatchley <chris@chrisblatchley.com>
2013-03-23 00:17:29 -0400

    bullets

--- js/game.js
+++ js/game.js
@@ -20,8 +20,13 @@ var ctx;
 // Create Global Game Variables
 var player;
 var spriteQueue = [];
+var laserQueue = [];
 var eventQueue = []
-var keysDown = new Array(222);	// A Boolean array to show keys down. 222 is Max keycode
+var keysDown = new Array(222);	// A Boolean array to show which keys are pressed
+
+//
+// Game Constants
+var Direction = { Up:0, Down:1, Left:2, Right: 3 };
 
 //*****************************************************************************
 //*****************************************************************************
@@ -64,7 +69,7 @@ function initGame() {
 	player.y = canvas.height / 2 - player.image.height / 2;
 	player.theta = 0;
 	player.forwardSpeed = 4;
-	player.rotateSpeed = 0.12;
+	player.rotateSpeed = 0.1;
 
 	$(document).keydown(function (e) { eventQueue.push(e); });
 	$(document).keyup(function (e) { eventQueue.push(e) });
@@ -80,6 +85,7 @@ function gameLoop() {
 
 		if(e.type == "keydown") {
 			keysDown[e.which] = true;
+			console.log(e.which);
 		}
 		else if(e.type == "keyup") {
 			keysDown[e.which] = false;
@@ -96,29 +102,45 @@ function gameLoop() {
 //
 // Update the automatic game state
 function updateGame() {
-	if(keysDown[37]) { // Left Arrow
-		player.theta -= player.rotateSpeed;
+	// Update Player Position
+	if(keysDown[65]) { // Left Arrow
+		movePlayer(Direction.Left);
 	}
-	if(keysDown[38]) { // Up Arrow
-		var dx = Math.sin(player.theta) * player.forwardSpeed;
-		var dy = Math.cos(player.theta) * player.forwardSpeed;
-		player.x += dx;
-		player.y -= dy;
+	if(keysDown[87]) { // Up Arrow
+		movePlayer(Direction.Up);
 	}
-	if(keysDown[39]) { // Right Arrow
-		player.theta += player.rotateSpeed;
+	if(keysDown[68]) { // Right Arrow
+		movePlayer(Direction.Right);
 	}
-	if(keysDown[40]) { // Down Arrow
-		var dx = Math.sin(player.theta) * player.forwardSpeed;
-		var dy = Math.cos(player.theta) * player.forwardSpeed;
-		player.x -= dx;
-		player.y += dy;
+	if(keysDown[83]) { // Down Arrow
+		movePlayer(Direction.Down);
 	}
-	if(keysDown[32]) { // Space
-		$("#message").html("Shoot!");
-	} else {
-		$("#message").html("");
+	if(keysDown[32]) {
+		shoot(player.x - player.image.width / 2,
+			  player.y - player.image.height / 2,
+			  player.theta);
 	}
+
+	// Update Laser Positions
+	for (var i = laserQueue.length - 1; i >= 0; i--) {
+		var l = laserQueue[i];
+		l.x += Math.sin(l.theta) * l.speed;
+	   	l.y -= Math.cos(l.theta) * l.speed
+
+	   	// Remove laser if out of bounds
+	   	if(l.x < 0 || l.x > canvas.width || l.y < 0 || l.y > canvas.width) {
+	   		laserQueue[i] = null;
+	   	}
+	};
+
+	// Remove old lasers
+	var newQueue = [];
+	for (var i = laserQueue.length - 1; i >= 0; i--) {
+		if(laserQueue[i] != null) {
+			newQueue.push(laserQueue[i]);
+		}
+	};
+	laserQueue = newQueue;
 }
 
 //
@@ -134,7 +156,57 @@ function drawScreen() {
 	ctx.translate( -(player.image.width/2), -(player.image.height/2) );
 	ctx.drawImage( player.image, 0, 0 );
 	ctx.restore();
-	
+
+	// Draw Lasers
+	ctx.strokeStyle = "red";
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+	for (var i = laserQueue.length - 1; i >= 0; i--) {
+		var l = laserQueue[i];
+		ctx.moveTo(l.x, l.y);
+		ctx.lineTo(l.x - Math.sin(l.theta) * l.size,
+				   l.y + Math.cos(l.theta) * l.size);
+	};
+	ctx.closePath();
+	ctx.stroke();
+}
+
+//*****************************************************************************
+//*****************************************************************************
+
+// Game Actions
+
+//
+// Move Player
+function movePlayer(direction) {
+	switch(direction)
+	{ 
+		case Direction.Left:
+			player.theta -= player.rotateSpeed;
+			break;
+		case Direction.Up:
+			var dx = Math.sin(player.theta) * player.forwardSpeed;
+			var dy = Math.cos(player.theta) * player.forwardSpeed;
+			player.x += dx;
+			player.y -= dy;
+			break;
+		case Direction.Right:
+			player.theta += player.rotateSpeed;
+			break;
+		case Direction.Down:
+			var dx = Math.sin(player.theta) * player.forwardSpeed;
+			var dy = Math.cos(player.theta) * player.forwardSpeed;
+			player.x -= dx;
+			player.y += dy;
+			break;
+	}
+}
+
+//
+// Shoot Action
+function shoot(x, y, theta) {
+	var laser = new Laser(x,y,theta);
+	laserQueue.push(laser);
 }
 
 //*****************************************************************************
@@ -146,4 +218,14 @@ function Sprite () {
 	this.x = 0;
 	this.y = 0;
 	this.image = null;
+}
+
+//
+// Basic Laser Object
+function Laser(x, y, theta) {
+	this.x = x;
+	this.y = y;
+	this.theta = theta;
+	this.size = 5;
+	this.speed = 15;
 }
