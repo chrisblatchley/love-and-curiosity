5aa07fbbe2a0abc7424a9eddd3c6c7604ce25be5
Chris Blatchley <chris@chrisblatchley.com>
2013-03-22 20:20:40 -0400

    added core files

---
+++ js/game.js
@@ -0,0 +1,26 @@
+/*****************************************
+ * File: game.js
+ * Authors: Chris Blatchley, Thad Bond
+ * 
+ * * * * * * * * * * * * * * * * * * * * *
+ * Love and Curiosity is a Javascript and
+ * Canvas game developed for the SP2013
+ * Gordon College Computer Science Club
+ * Programming Competition. We hope you
+ * enjoy playing Love and Curiosity.
+ *****************************************/
+
+//
+// Create Document Variables
+var canvas;
+var ctx;
+
+
+//
+// Create Global Game Variables
+var player;
+var spr
+
+$(function () {
+	$("#message").html("Hi there");
+});
